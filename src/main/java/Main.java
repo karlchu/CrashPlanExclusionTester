acdcea7f5674f4c0e3fe0939cbacf9dd4ea5c324
Karl Chu <kchu13@gmail.com>
2014-04-19 14:45:49 +1000

    Renamed Handler to PathFilter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,23 +8,23 @@ public class Main {
             return;
         }
         File root = new File(args[0]);
-        Handler handler = new Handler(args[1]);
-        listAllFilesRecursively(root, handler);
+        PathFilter pathFilter = new PathFilter(args[1]);
+        listAllFilesRecursively(root, pathFilter);
 
-        System.out.printf("# Total size: %d\n", handler.getTotalSize());
+        System.out.printf("# Total size: %d\n", pathFilter.getTotalSize());
     }
 
     private static void printUsage() {
         System.out.print("java Main <path_to_scan> <regex_to_scan>");
     }
 
-    private static void listAllFilesRecursively(File file, Handler handler) {
-        handler.handle(file);
+    private static void listAllFilesRecursively(File file, PathFilter pathFilter) {
+        pathFilter.filterAndHandle(file);
         if (file.isDirectory()) {
             File[] files = file.listFiles();
             if (files == null) return;
             for (File f : files) {
-                listAllFilesRecursively(f, handler);
+                listAllFilesRecursively(f, pathFilter);
             }
         }
     }
--- src/main/java/PathFilter.java
+++ src/main/java/PathFilter.java
@@ -1,11 +1,11 @@
 import java.io.File;
 import java.util.regex.Pattern;
 
-public class Handler {
+public class PathFilter {
     private long totalSize = 0L;
     private final Pattern pattern;
 
-    public Handler(String regex) {
+    public PathFilter(String regex) {
         pattern = Pattern.compile(regex);
     }
 
@@ -13,7 +13,7 @@ public class Handler {
         return totalSize;
     }
 
-    public void handle(File file) {
+    public void filterAndHandle(File file) {
         String path = file.getAbsolutePath();
         if (!pattern.matcher(path).matches()) {
             if (file.isFile()) totalSize += file.length();
